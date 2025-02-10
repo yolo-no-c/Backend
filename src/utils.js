16538bad19228bc51a6167b1dc73d193a17085a5
yolo-no-c <yolonoc2006@gmail.com>
2025-02-09 22:48:42 -0500

    se realizan ajustes para entrega final

---
+++ src/utils.js
@@ -0,0 +1,4 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url) // nos da la ruta desde donde se esta haciendo el import
+export const __dirname = dirname(__filename)
